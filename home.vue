d687d515f76a597a525205a1b99446b248068db5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-28 17:01:36 -0400

    The Toronto Path 1.0  /home.vue

---
+++ home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row">
+    <div class="large-6 columns">
+      <div>
+        <h1>{{title}}</h1>
+        <p>{{description}}</p>
+        <hr/>
+        <h5>Translated text example:</h5>
+        <p>{{ $t("message.hello") }}</p>
+        <p>{{property.name}}</p>
+        <hr/>
+        <today-hours></today-hours>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  define(["Vue", "vue!today_hours"], function(Vue, TodayHoursComponent) {
+    return Vue.component("home-component", {
+      template: template, // the variable template will be injected
+      data: function() {
+        return {
+          title: The Path",
+          description: "An example of integration of Mall Maverick with Vue.js"
+        }
+      },
+      computed: {
+        property(){
+          return this.$store.getters.getProperty;
+        }
+      }
+    })
+  })
+</script>
