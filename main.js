8c474b5bdcf84df9f0b49986313722d0c4cbf67b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-29 17:50:44 -0400

    The Toronto Path 1.0  /main.js

--- main.js
+++ main.js
@@ -46,7 +46,8 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
     data: function () {
       return {
         dataLoaded: false,
-        test: ''
+        test: '',
+        newsletter_control: ''
       }
     },
     created() {
