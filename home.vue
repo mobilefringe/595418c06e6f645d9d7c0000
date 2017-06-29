9045ca006e8408e347a94340923938b770d2f9b5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-29 17:13:00 -0400

    The Toronto Path 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -19,7 +19,7 @@
                 <!--<form id="mobile_search_form" class="no_margin">-->
                 <!--    <input id="mobile_search_box" type="text" placeholder="Search All Path Stores" title="search" />-->
                 <!--</form>-->
-                <input v-model="mobile_search_box" placeholder="Search All Path Stores" >
+                <input v-model="mobile_search_box" id="mobile_search_box" placeholder="Search All Path Stores" >
             </div>
             <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498575328000/Search Icon.png" class="search_icon" alt="">
         </div>
