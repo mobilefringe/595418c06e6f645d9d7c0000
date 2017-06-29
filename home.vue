61d5b77726573a2d14bb449fc398cb1d5858802f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-29 16:53:59 -0400

    The Toronto Path 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -39,7 +39,7 @@
                 <a class="all_caps menu_btn_label">YOUR LOCATION</a>
             </router-link>
             <router-link tag="li" to="/stores" active-class="active" class="all_caps menu_btn" exact>
-                  <a class="all_caps menu_btn_label">STORES</a>
+                  <a class="all_caps menu_btn_label">STORESS</a>
             </router-link>
             <router-link tag="li" to="/promotions" active-class="active" class="all_caps menu_btn" exact>
                   <a class="all_caps menu_btn_label">PROMOTIONS</a>
