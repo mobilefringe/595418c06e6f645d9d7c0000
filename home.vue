b6dc30be9f5fae9b46d6b88da05de50546a450ab
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-29 10:43:25 -0400

    The Toronto Path 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="row">
-    <div class="large-6 columns">
-      <div>
-        <h1>{{title}}</h1>
-        <p>{{description}}</p>
-        <hr/>
-        <h5>Translated text example:</h5>
-        <p>{{ $t("message.hello") }}</p>
-        <p>{{property.name}}</p>
-        <hr/>
-        <today-hours></today-hours>
-      </div>
-    </div>
-  </div>
+  <!--<div class="row">-->
+  <!--  <div class="large-6 columns">-->
+  <!--    <div>-->
+  <!--      <h1>{{title}}</h1>-->
+  <!--      <p>{{description}}</p>-->
+  <!--      <hr/>-->
+  <!--      <h5>Translated text example:</h5>-->
+  <!--      <p>{{ $t("message.hello") }}</p>-->
+  <!--      <p>{{property.name}}</p>-->
+  <!--      <hr/>-->
+  <!--      <today-hours></today-hours>-->
+  <!--    </div>-->
+  <!--  </div>-->
+  <!--</div>-->
 </template>
 
 <script>
