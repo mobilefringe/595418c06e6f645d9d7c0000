eef5c6fc8fe11d2b3cf7ba4716bb43c60801ecc5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-29 17:16:59 -0400

    The Toronto Path 1.0  /stores.vue

---
+++ stores.vue
@@ -0,0 +1,203 @@
+<template>
+<div class="stores_page">
+    <div class="all_caps store_header">
+        Stores Directory
+    </div>
+    <h4 class="all_caps home_title"> Discover The Toronto Path</h4>
+    <div class="store_cat_listing">
+        <div class="all_caps cat_title" id="dine">
+            Dine
+            <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right hidden_now" alt="">
+        </div>
+         <hr>
+        <div class="hidden_now" id="dine_store_list">
+            <div class="" id="dine_stores_container">
+                <script id="dine_stores_template" type="x-tmpl-mustache/text">
+                    <div class="row cat_name">
+                        <div class="col-xs-6 align_left">
+                            {{name}} 
+                            <br/>
+                            Building Name
+                        </div>
+                        <div class="col-xs-6 align_right">
+                            April 27-29
+                        </div>
+                    </div>
+                     <hr>
+                </script>
+            </div>
+        </div>
+       
+        <div class="all_caps cat_title" id="shop">
+            Shop
+            <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right hidden_now" alt="">
+        </div>
+         <hr>
+        <div class="hidden_now" id="shop_store_list">
+            <div class="" id="shop_stores_container">
+                <script id="shop_stores_template" type="x-tmpl-mustache/text">
+                    <div class="row cat_name">
+                        <div class="col-xs-6 align_left">
+                            {{name}} 
+                            <br/>
+                            Building Name
+                        </div>
+                        <div class="col-xs-6 align_right">
+                            April 27-29
+                        </div>
+                    </div>
+                     <hr>
+                </script>
+            </div>
+        </div>
+        <div class="all_caps cat_title" id="play">
+            Play
+            <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right hidden_now" alt="">
+        </div>
+        <hr>
+        <div class="hidden_now" id="play_store_list">
+            <div class="" id="play_stores_container">
+                <script id="play_stores_template" type="x-tmpl-mustache/text">
+                    <div class="row cat_name">
+                        <div class="col-xs-6 align_left">
+                            {{name}} 
+                            <br/>
+                            Building Name
+                        </div>
+                        <div class="col-xs-6 align_right">
+                            April 27-29
+                        </div>
+                    </div>
+                     <hr>
+                </script>
+            </div>
+        </div>
+        <div class="all_caps cat_title" id="service">
+            Service
+            <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right hidden_now" alt="">
+        </div>
+         <hr>
+        <div class="hidden_now" id="service_store_list">
+            <div class="" id="service_stores_container">
+                <script id="service_stores_template" type="x-tmpl-mustache/text">
+                    <div class="row cat_name">
+                        <div class="col-xs-6 align_left">
+                            {{name}} 
+                            <br/>
+                            Building Name
+                        </div>
+                        <div class="col-xs-6 align_right">
+                            April 27-29
+                        </div>
+                    </div>
+                     <hr>
+                </script>
+            </div>
+        </div>
+        <div class="all_caps cat_title" id="stay">
+            Stay
+            <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right hidden_now" alt="">
+        </div>
+         <hr>
+        <div class="hidden_now" id="stay_store_list">
+            <div class="" id="stay_stores_container">
+                <script id="stay_stores_template" type="x-tmpl-mustache/text">
+                    <div class="row cat_name">
+                        <div class="col-xs-6 align_left">
+                            {{name}} 
+                            <br/>
+                            Building Name
+                        </div>
+                        <div class="col-xs-6 align_right">
+                            April 27-29
+                        </div>
+                    </div>
+                     <hr>
+                </script>
+            </div>
+        </div>
+    </div>
+    <!--<div id="category_list_container">-->
+    <!--    <script id="category_list_template" type="x-tmpl-mustache/text">-->
+    <!--        {{name}}-->
+    <!--    </script>-->
+        
+    <!--</div>-->
+</div>
+
+  <div class="container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
+    <ul class="menu">
+      <li><a v-on:click="changeMode('alphabetical')">Alphabetical</a></li>
+      <li><a v-on:click="changeMode('category')">Category</a></li>
+    </ul>
+    <hr/>
+    <div class="columns large-12" v-for="(stores, index) in storesByAlphaIndex" v-if="listMode === 'alphabetical'">
+      <div class="list_header">
+        <b>{{index}}</b>
+        <hr/>
+      </div>
+      <div class="store-section" v-for="store in stores">
+        <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">{{store.name}}</router-link>
+        <hr/>
+      </div>
+      <!-- <div class="card">
+        <div class="card-divider">
+          {{ store.name }}
+        </div>
+        <div class="card-section center">
+          <a :href="store.image_url" target="_blank"><img :src="store.image_url"></a>
+        </div>
+        <div class="card-section">
+          <div class="center">
+            <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">View Details</router-link>
+          </div>
+        </div>
+      </div> -->
+    </div>
+    <div class="columns large-12" v-for="(stores, index) in storesByCategoryName" v-if="listMode === 'category'">
+      <div class="list_header">
+        <b>{{index}}</b>
+        <hr/>
+      </div>
+      <div class="store-section" v-for="store in stores">
+        <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">{{store.name}}</router-link>
+        <hr/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+  .center{
+    text-align: center
+  }
+  .store-section a{
+    color: #708090;
+  }
+</style>
+
+<script>
+  define(["Vue"], function(Vue) {
+    return Vue.component("stores-component", {
+      template: template, // the variable template will be injected
+      data: function() {
+        return {
+          listMode: "alphabetical"
+        }
+      },
+      methods: {
+        changeMode (mode) {
+          this.listMode = mode;
+        }
+      },
+      computed: {
+        storesByAlphaIndex() {
+          return this.$store.getters.storesByAlphaIndex;
+        },
+        storesByCategoryName() {
+          return this.$store.getters.storesByCategoryName;
+        }
+      }
+    });
+  });
+</script>
