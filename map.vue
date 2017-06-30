069f15d4a3021037adbf7435b0b9ca684ef24d47
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-30 14:04:17 -0400

    The Toronto Path 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -17,7 +17,7 @@
             
                 <div class="demo_1 map3" style="">       
                 
-                    <img alt="map_image" id="map_image" v-attr="src: getSVGurl" >   
+                    <img alt="map_image" id="map_image"<img v-bind:src="imageSrc"> > 
                     <!--v-bind:src={{getSVGurl}} src="//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1498505789000/background image_menu.png"-->
                 </div>
             </div>
