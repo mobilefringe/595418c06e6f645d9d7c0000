294b4067b8df626981119f80605a6d5ae6857df1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-30 15:08:07 -0400

    The Toronto Path 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -64,7 +64,7 @@
             if (this.mobile_search_box.trim() === '')
             this.result = this.list
             else
-            this.result = this.fuse.search(this.search.trim())
+            this.result = this.fuse.search(this.mobile_search_box.trim())
         }
       },
       computed: {
