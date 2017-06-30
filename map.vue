72f69f84246f8fb1cebbf0c4db3d03ef66723d7b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-30 14:05:42 -0400

    The Toronto Path 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -34,7 +34,7 @@
             title: "The Path!",
             description: "Using new SDK",
             mobile_search_box : "",
-            svg_map_url: getSVGurl//"//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1498505789000/background image_menu.png"
+            svg_map_url: getSVGurl()//"//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1498505789000/background image_menu.png"
         }
       },
       computed: {
