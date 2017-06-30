310d29fe2fd0f54021f0fc6701f5ac1d175a0b3d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-30 15:40:57 -0400

    The Toronto Path 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -6,7 +6,7 @@
                     <input v-model="mobile_search_store" class= "mobile_map_search" placeholder="Search Stores" >
                     <input v-model="mobile_find_store" class= "mobile_map_search" placeholder="Find a store near you" >
                 </div>
-                <img @click:swapSearchterm src="//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1498848398000/Arrows Icon.png" class="search_icon" alt="">
+                <img @click="swapSearchterm" src="//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1498848398000/Arrows Icon.png" class="search_icon" alt="">
             </div>
             <div id="search_results_items" class="search-results-items" style="">
             <!--RENDERED BY renderSearchResultsTemplate(..) -->
