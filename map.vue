39df3cb7d1ae471acf56db95428d925697777aee
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-04 14:06:45 -0400

    The Toronto Path 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -23,7 +23,7 @@
                             <img @click="removeSearch1" src="//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1499180586000/Close_button.png" class="" alt="">
                         </div>
                         <div class="more_dets pull-right">
-                            <router-link to="{{store1.slug}}"><img @click="viewMoreDets1" src="//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1499180555000/Right Arrow.png" class="" alt=""></router-link>
+                            <router-link to="store1.slug"><img @click="viewMoreDets1" src="//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1499180555000/Right Arrow.png" class="" alt=""></router-link>
                         </div>
                         <p class="dets_title">{{store1.name}} </p>
                         
