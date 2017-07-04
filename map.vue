b3152b715bbc2e5881ab6e1137fb8d8143978a85
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-04 12:17:17 -0400

    The Toronto Path 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -21,10 +21,11 @@
                 </div>
                 <div class="blue_dets_box hidden_now">
                     <div class="close_dets pull-left">
+                        <span class="Centerer"></span>
                         <img @click="removeSearch1" src="//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1499180586000/Close_button.png" class="" alt="">
                     </div>
                     <div class="more_dets pull-right">
-                    <span class="Centerer"></span>
+                        <span class="Centerer"></span>
                         <img @click="viewMoreDets1" src="//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1499180555000/Right Arrow.png" class="" alt="">
                     </div>
                     <p class="dets_title">{{mobile_search_store}} </p>
@@ -32,7 +33,7 @@
                 </div>
                 <div class="green_dets_box hidden_now">
                     <div class="close_dets pull-left">
-                    <span class="Centerer"></span>
+                        <span class="Centerer"></span>
                         <img @click="removeSearch2" src="//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1499180586000/Close_button.png" class="" alt="">
                     </div>
                     <div class="more_dets pull-right">
