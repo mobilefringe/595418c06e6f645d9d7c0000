a400ad7a7a0146036f0e46f15362f29660d1c834
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-05 12:44:10 -0400

    The Toronto Path 1.0  /main.js

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
             name: "Home",
             id: "home",
             li_class_list : "",
-            a_class_list:"all_caps menu_btn_label"
+            a_class_list:"all_caps menu_btn_label",
             is_inHome: false
         }]
       }
