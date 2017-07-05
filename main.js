b0418870ce2cfa22bb4ab1c4f572cf4832f3ed1f
ikvir@mobilefringe.com @ CodeCloud <ikvir@mobilefringe.com @ CodeCloud>
2017-07-05 13:50:03 -0400

    The Toronto Path 1.0  /main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 require.config({
   paths: {
-    'Vue': 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.3.3/vue.min',
+    'Vue': 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.3.4/vue.min',
     'vue_router': 'https://cdnjs.cloudflare.com/ajax/libs/vue-router/2.5.2/vue-router.min',
     'axios': 'https://cdnjs.cloudflare.com/ajax/libs/axios/0.16.1/axios.min',
     'jquery': 'https://code.jquery.com/jquery-3.2.1.min',
@@ -8,7 +8,7 @@ require.config({
     'moment': 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment-with-locales.min',
     'moment-timezone': 'https://momentjs.com/downloads/moment-timezone-with-data-2012-2022.min',
     'vue2-filters': 'https://cdn.jsdelivr.net/vue2-filters/0.1.8/vue2-filters.min',
-    'vue': 'https://cdn.rawgit.com/edgardleal/require-vuejs/aeaff6db/dist/require-vuejs.min',
+    'vue': 'https://cdn.rawgit.com/edgardleal/require-vuejs/master/dist/require-vuejs.min',
     'vuex': 'https://cdnjs.cloudflare.com/ajax/libs/vuex/2.3.1/vuex.min',
     'vue-i18n': 'https://cdnjs.cloudflare.com/ajax/libs/vue-i18n/6.1.1/vue-i18n.min',
     'text': 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min',
