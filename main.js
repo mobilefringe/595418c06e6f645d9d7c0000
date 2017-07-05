de347f1d8652cbbe91259ad5b93836801cb4b1e5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-05 12:29:47 -0400

    The Toronto Path 1.0  /main.js

--- main.js
+++ main.js
@@ -90,6 +90,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
         this.locale = val; // this will update the data store, which in turn will trigger the watcher to update the locale in the system
       },
       open_menu: function () {
+          console.log(copyright_year);
 		$(this).toggleClass('open');
 		$('#menu_page').show();
 		$('#menu-icon').toggle();
