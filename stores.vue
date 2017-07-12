bc3a993caace2fec4ad782896279266531c78c95
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-12 11:44:08 -0400

    The Toronto Path 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -139,9 +139,6 @@
                 this.show_service = !this.show_service;
         },
         getStoreByCategory() {
-            //toggle value of show_dine
-            //this.show_dine = !this.show_dine;
-            console.log(this.show_dine);
             var cats = this.storesByCategoryName;
             console.log(cats);
             var dine_cats = ["Food", "Fast Food"];
