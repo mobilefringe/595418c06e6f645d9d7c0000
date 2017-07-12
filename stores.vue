c1d6749298d8d9794618bfb526f4a33d8a5aef5e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-12 11:06:58 -0400

    The Toronto Path 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -29,7 +29,6 @@
                 </div>
                 <hr>
             </div>
-        </div>
             <div class="all_caps cat_title" id="shop">
                 Shop
                 <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right hidden_now" alt="">
