a965f8696665a3d5b37296b6ddb637a2e0e7071a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-12 11:08:00 -0400

    The Toronto Path 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -10,7 +10,7 @@
                 <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right hidden_now" alt="">
             </div>
              <hr>
-            <div  v-show = "dine_stores" class="hidden_now" id="dine_store_list">
+            <div  v-show = "show_dine" class="hidden_now" id="dine_store_list">
                 <div class="" id="dine_stores_container">
                     <div class="row cat_name">
                         <div  v-for="store in dine_stores">
