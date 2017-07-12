b2d4f0373d13f02de8e0a9e9b6de43c5ab39932d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-12 11:06:03 -0400

    The Toronto Path 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -13,15 +13,15 @@
             <div  v-show = "dine_stores" class="hidden_now" id="dine_store_list">
                 <div class="" id="dine_stores_container">
                     <div class="row cat_name">
-                    <div  v-for="store in dine_stores">
-                        <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">{{store.name}}</router-link>
-                        <hr/>
-                    </div>
-                     <div class="col-xs-6 align_left">
-                         {{name}} 
-                        <br/>
-                         Building Name
-                     </div>
+                        <div  v-for="store in dine_stores">
+                            <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">{{store.name}}</router-link>
+                            <hr/>
+                        </div>
+                        <div class="col-xs-6 align_left">
+                             {{name}} 
+                            <br/>
+                             Building Name
+                        </div>
                      <div class="col-xs-6 align_right">
                         April 27-29
                      </div>
