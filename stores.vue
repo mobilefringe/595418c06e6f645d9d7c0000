489608d1d32b803a0157f0687f80c67d7c0a923a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-12 11:12:00 -0400

    The Toronto Path 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -14,9 +14,6 @@
                 <div class="" id="dine_stores_container">
                     <div class="row cat_name">
                         <div  v-for="store in dine_stores">
-                            <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">{{store.name}}</router-link>
-                            <hr/>
-                        
                             <div class="col-xs-6 align_left">
                                  {{name}} 
                                 <br/>
