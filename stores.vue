4aa6a9e4421633fcb20a35a9c5aebca0b42d7512
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-13 11:43:18 -0400

    The Toronto Path 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -126,7 +126,7 @@
 <!--</style>-->
 
 <script>
-  define(["Vue","lodash"], function(Vue) {
+  define(["Vue","lodash"], function(Vue,_) {
     return Vue.component("stores-component", {
       template: template, // the variable template will be injected
       data: function() {
