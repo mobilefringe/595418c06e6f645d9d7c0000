6b0a958dcbd8e30f4bcdacb99b628aa9d0ca744c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-19 15:40:20 -0400

    The Toronto Path 1.0  /map_stores.vue

--- map_stores.vue
+++ map_stores.vue
@@ -40,14 +40,14 @@
                     <!--    <p class="dets_data">Building Name {{store2.name}}</p>-->
                         
                     <!--</div>-->
-                    <div v-for = "store in all_stores">
+                    <!--<div v-for = "store in all_stores">-->
                         <!--<div class="marker" id="store_{{store.id}}" data-coords="{{store.x_coordinate}} , {{store.y_coordinate}}">-->
                         <!--    <a style="color:#fff" href=/stores/store.slug>  store.name  </a>-->
                         <!--</div>-->
-                        <router-link tag="div" :to="{ name: 'storeDetails', params: { id: store.slug }}" :data-coords="store.coordinate"  class="marker">
-                           <a style="color:#fff" >  {{store.name}}  </a>
-                        </router-link>
-                    </div>
+                    <!--    <router-link tag="div" :to="{ name: 'storeDetails', params: { id: store.slug }}" :data-coords="store.coordinate"  class="marker">-->
+                    <!--       <a style="color:#fff" >  {{store.name}}  </a>-->
+                    <!--    </router-link>-->
+                    <!--</div>-->
                     
                 </div>
             </div>
