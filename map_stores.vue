4433387b7208e5b54688b6f1b6c39301d5560a92
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-20 14:01:02 -0400

    The Toronto Path 1.0  /map_stores.vue

--- map_stores.vue
+++ map_stores.vue
@@ -139,17 +139,17 @@
                         // ]);
                         console.log($("#"+val.svgmap_region));
                         // var coords = map.get_coords(val.svgmap_region);
-                        var height = parseInt(coords["height"])
-                        var width = parseInt(coords["width"])
-                        var x_offset = (parseInt(width) / 2);
-                        var y_offset = (parseInt(height) /2);
+                        // var height = parseInt(coords["height"])
+                        // var width = parseInt(coords["width"])
+                        // var x_offset = (parseInt(width) / 2);
+                        // var y_offset = (parseInt(height) /2);
                         
-                        map.setMarks([{ xy: [coords["x"] - 15 + x_offset, coords["y"] - 55 + y_offset],
-                                  attrs: {
-                                            src:   '//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1500567644000/map_pin_1x.png'   // image for marker
-                                          }
-                            }
-                            ])
+                        // map.setMarks([{ xy: [coords["x"] - 15 + x_offset, coords["y"] - 55 + y_offset],
+                        //           attrs: {
+                        //                     src:   '//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1500567644000/map_pin_1x.png'   // image for marker
+                        //                   }
+                        //     }
+                        //     ])
                         // map.setViewBox(val.svgmap_region);
                         // map.selectRegion(val.svgmap_region);
                     }
