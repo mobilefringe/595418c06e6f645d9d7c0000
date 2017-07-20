5f67d9b5545ccac3e5cfec3733ccbd74ffc14496
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-20 10:05:23 -0400

    The Toronto Path 1.0  /map_stores.vue

--- map_stores.vue
+++ map_stores.vue
@@ -7,7 +7,7 @@
                 
                     <img alt="map_image" id="map_image" class="map_image" :src="getSVGurl"> 
                     
-                    <div v-for = "store in all_stores" class="marker" style="display: inline-block;" :id="concatVal('store_',store.id)" :data-coords="concatCood(store.x_coordinate, store.y_coordinate)">
+                    <div v-for = "store in all_stores" class="marker" :id="concatVal('store_',store.id)" :data-coords="concatCood(store.x_coordinate, store.y_coordinate)">
                        
                             <a  style="color:#fff" :href="concatVal('/stores/',store.slug)">  {{store.name}}  </a>
                        
