89c2d83de6c29c9e99aefed7131d07120bfa951e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-21 16:04:05 -0400

    The Toronto Path 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -69,7 +69,12 @@
         methods : {
             onOptionSelect(option) {
                 console.log('Selected option:', option)
-            }
+            },
+        removeDuplicates(myArr, prop) {
+            return myArr.filter((obj, pos, arr) => {
+                return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
+            });
+        }
         }
     })
   })
