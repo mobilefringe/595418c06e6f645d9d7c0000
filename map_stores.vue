98c1e8499e66dc7eb85c0d3fdc3cdccc87120e86
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-21 10:12:54 -0400

    The Toronto Path 1.0  /map_stores.vue

--- map_stores.vue
+++ map_stores.vue
@@ -145,7 +145,7 @@
                     // if(b.id.indexOf("stores_x5F") > -1)
                     //console.log(b.id);
                     var a = "stores_x5F_6_x5F_18_4_";
-                    if(b.id==a){
+                    if(a.indexOf(b.id) > -1){
                         console.log(b.id);
                         return b;
                     }
