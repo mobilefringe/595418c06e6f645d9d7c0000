602f00697dc4f080b2278abf2713a8bb1cc68846
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-21 15:49:29 -0400

    The Toronto Path 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -34,7 +34,7 @@
 </template>
 
 <script>
-  define(["Vue","vue!search-component"], function(Vue) {
+  define(["Vue","vue!search-component"], function(Vue,SearchComponent) {
     return Vue.component("home-component", {
         props: ['menu_items'],
         template: template, // the variable template will be injected
