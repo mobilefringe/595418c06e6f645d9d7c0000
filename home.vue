c81f240e009d93d3661ae75eda0ea97c0cea3ef4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-24 15:18:08 -0400

    The Toronto Path 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -72,7 +72,7 @@
             onOptionSelect(option) {
                 console.log('Selected option:', option);
                 var counted_stores = _.countBy(this.allStores,'name');
-                console.log(counted_stores[option]);
+                console.log(counted_stores[option.name]);
                 // if( option)
                 // router.go(/map_details)
             },
