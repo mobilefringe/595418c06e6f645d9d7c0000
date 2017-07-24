51d2c7bc74f57c83fe0f7de1efe1eb9ea3fe1a2b
ikvir@mobilefringe.com @ CodeCloud <ikvir@mobilefringe.com @ CodeCloud>
2017-07-24 14:37:03 -0400

    The Toronto Path 1.0  /main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ require.config({
     'moment': 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment-with-locales.min',
     'moment-timezone': 'https://momentjs.com/downloads/moment-timezone-with-data-2012-2022.min',
     'vue2-filters': 'https://cdn.jsdelivr.net/vue2-filters/0.1.8/vue2-filters.min',
-    'vue': 'https://cdn.rawgit.com/edgardleal/require-vuejs/aeaff6db/dist/require-vuejs.min',
+    'vue': 'https://mmvue.codecloudapp.com/require-vuejs.min',
     'vuex': 'https://cdnjs.cloudflare.com/ajax/libs/vuex/2.3.1/vuex.min',
     'vue-i18n': 'https://cdnjs.cloudflare.com/ajax/libs/vue-i18n/6.1.1/vue-i18n.min',
     'text': 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min',
