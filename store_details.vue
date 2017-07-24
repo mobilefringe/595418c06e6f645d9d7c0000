0e191bbc4ba05ee7043ab6d995598708cb2c1195
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-24 16:41:09 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -193,6 +193,10 @@
         findStoreBySlug () {
           return this.$store.getters.findStoreBySlug;
         }
+      }, 
+      methods : function () {
+         getStoresByName(map) {
+         }
       }
     });
   });
