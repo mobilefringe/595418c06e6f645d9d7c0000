b178d5a3ee1d97a7ba53d9866e802fdc4405d19e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-24 16:54:31 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -70,7 +70,7 @@
       }, 
       methods : {
          loadMap() {
-            this.map = $('#mapsvg').mapSvg({
+             $('#mapsvg').mapSvg({
                     source: this.getSVGurl,    // Path to SVG map
                     colors: {stroke: '#aaaaaa', selected: "#CC00CC", hover: "#CC00CC"},
                     // viewBox: [3000,0,6000,6000],
