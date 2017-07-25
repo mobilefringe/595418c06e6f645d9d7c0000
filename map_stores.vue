3eb6ae986c84ee02966b528a520333ad6c00c6db
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-25 12:59:20 -0400

    The Toronto Path 1.0  /map_stores.vue

--- map_stores.vue
+++ map_stores.vue
@@ -126,7 +126,7 @@
                         var x_offset = (parseInt(width) / 2);
                         var y_offset = (parseInt(height) /2);
                         
-                        map.setMarks([{ xy: [coords["x"]    + x_offset, coords["y"] -68 + y_offset],
+                        map.setMarks([{ xy: [coords["x"]    + x_offset, coords["y"] -34 + y_offset],
                             attrs: {
                                 src: '//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1500567644000/map_pin_1x.png', // image for marker
                                 href: '/stores/'+val.slug
