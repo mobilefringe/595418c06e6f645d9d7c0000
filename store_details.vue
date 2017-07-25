359120f661996ed2a2f7a15f3a1c975d243ba78b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-25 11:54:51 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -42,23 +42,7 @@
         }
       },
       mounted : function (){
-          var map = $('#mapsvg').mapSvg({
-                    source: this.getSVGurl,    // Path to SVG map
-                    colors: {stroke: '#aaaaaa', selected: "#CC00CC", hover: "#CC00CC"},
-                    // viewBox: [3000,0,6000,6000],
-                    disableAll: true,
-                    height:1000,
-                    width:1300,
-                    tooltipsMode:'custom',
-                    loadingText: "loading...",
-                    zoom: true,
-                    zoomButtons: {'show': true,'location': 'left' },
-                    pan:true,
-                    cursor:'pointer',
-                    responsive:true,
-                    zoomLimit: [0,10]
-                });
-        this.loadMap(map);
+          
       },
       watch: {
             currentStore : function () {
