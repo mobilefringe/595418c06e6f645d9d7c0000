4e924274eb7aa67d7249715db0a9ac0445757b08
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-27 13:55:53 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -120,6 +120,7 @@
             all_hours : function () {
                 console.log(this.all_hours);
                 this.store_hours =  _.find(this.all_hours, { 'id': this.currentStore.todays_hour}); 
+                 console.log(this.store_hours);
             }
         },
       computed: {
