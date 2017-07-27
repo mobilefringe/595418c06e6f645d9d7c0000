2083b66c0033a2bf152e6cb3459dcb8745d076ee
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-27 13:41:05 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -160,7 +160,7 @@
             // console.log(property);
             let timezone = property.timezone_moment;
             let all_hours =this.state.results.hours;
-            // console.log("all_hours", all_hours);
+            console.log("all_hours", all_hours);
             let todayHours = _.find(all_hours, { 'id': store_hour_id});//hours.find(hour => hour.day_of_week === moment().day());
             console.log("todayHours", todayHours);
             return todayHours;
