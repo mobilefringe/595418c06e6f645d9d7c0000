4af6fcfa14eaac1ff6a9300f56de542168558512
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-27 15:37:11 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -16,10 +16,10 @@
         width:25px;
     }
     .store_dets {
-    position: relative;
-    left: initial;
-    bottom: initial;
-    z-index: 1;
+        position: relative;
+        left: initial;
+        bottom: initial;
+        z-index: 1;
     }
 }
 </style>
