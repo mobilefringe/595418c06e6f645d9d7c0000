1af85570fa12624d171435f4a823dda6d2d80632
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-27 13:47:58 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -39,7 +39,7 @@
             </div>
             <p class="dets_title">{{currentStore.name | uppercase}} </p>
             <p class="dets_data">{{currentStore.property_id}}</p>
-            <p class="dets_data">{{getTodaysStoreHours()}}</p>
+            <p class="dets_data">{{store_hours}}</p>
         </div>
   </div>
 </template>
