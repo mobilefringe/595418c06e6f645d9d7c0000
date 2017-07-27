44603ec98a4bff93b050c9c0021b7b368578636e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-27 14:43:00 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -122,7 +122,7 @@
                 console.log(this.currentStore.todays_hour);
                 // this.store_hours =  _.find(this.all_hours, 'id', this.currentStore.todays_hour);
                 _.forEach(this.all_hours, function(val, key) {
-                    console.log(val.id);
+                    ??console.log(val.id);
                     if(val.id == this.currentStore.todays_hour)
                     {
                         console.log("HELLO WORLD", val);
