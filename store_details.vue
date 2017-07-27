f7506319aa2e0d0eb15ae1b2198e953443395c6b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-27 12:24:16 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -53,7 +53,8 @@
           title: "The Path!",
           description: "An example of integration of Mall Maverick with Vue.js",
           currentStore: null,
-          map: {}
+          map: {},
+          todays_hour:""
         }
       },
       beforeRouteEnter (to, from, next) {
