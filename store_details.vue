b9653b45c8bbb8582636532efc30a5429c19950a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-27 16:29:49 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -47,16 +47,15 @@
                     store_hours: {}
                 }
             },
-      beforeRouteEnter (to, from, next) {
-        next(vm => {
-          // access to component instance via `vm`
-          vm.currentStore = vm.findStoreBySlug(to.params.id);
-          this.currentStore = vm.currentStore;
-        //   console.log(this.currentStore);
-          if (vm.currentStore === null || vm.currentStore === undefined){
-            vm.$router.replace({ name: '404'});
-          }
-        })
+            beforeRouteEnter (to, from, next) {
+            next(vm => {
+              // access to component instance via `vm`
+              vm.currentStore = vm.findStoreBySlug(to.params.id);
+              this.currentStore = vm.currentStore;
+              if (vm.currentStore === null || vm.currentStore === undefined){
+                vm.$router.replace({ name: '404'});
+              }
+            })
       },
       beforeRouteUpdate (to, from, next) {
         this.currentStore = this.findStoreBySlug(to.params.id);
