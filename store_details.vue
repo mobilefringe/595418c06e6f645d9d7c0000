e9670c723927625d8c52e5fe6fed6882d7e1cc8b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-27 17:05:54 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -114,8 +114,9 @@ define(["Vue", "jquery", "mm_mapsvg","mousewheel","raphael", "moment", "moment-t
                 });
                 this.loadMap(map);
                 this.all_hours = this.state.results.hours; 
-                console.log(this.promotions());
+                
                 var store_promo =  this.promotions();
+                console.log(store_promo);
                 _.forEach(this.currentStore.promotions, function(val, key) {
                     _.find(this.promotions(), 'id', this.currentStore.todays_hour);
                     Vue.set(this.all_promos,key, this.findPromoBySlug(val));
