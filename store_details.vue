73132d302dd4dfa15719c42c7ea992953fa9ec3d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-28 10:42:14 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -42,7 +42,7 @@
             <!--</ul>-->
             <div class="row cat_name" v-if="currentStore.total_published_promos > 0">
                 <div class=" align_left" v-for="promo in all_promos">
-                    <p class="col-xs-12"></p>{{currentStore.name | uppercase}}
+                    <p class="col-xs-12">{{currentStore.name | uppercase}}</p>
                     <br/>
                      {{promo.name}} - {{promo.date}}
                 </div>
