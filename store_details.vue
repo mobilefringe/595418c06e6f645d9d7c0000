8adfdfd580b3397e942b98945f88100f9d566f57
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-28 10:59:51 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -48,7 +48,7 @@
                         {{promo.name}}
                     </p>
                     
-                    <p class="col-xs-6 pull-right">
+                    <p class="col-xs-6 pull-right align_right">
                         {{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}
                     </p>
                     <hr/>
