a832fdd48b9140e48fb41631f1fba707a4de0891
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-28 16:09:10 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -5,7 +5,7 @@
         </div>
         <div class="blue_box"  v-if="currentStore">
             <div class="close_dets  pull-left" @click="go_back">
-                <a :href="">
+                <a href="">
                     <img src="//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1501003074000/arrow_left_2x.png" class="" alt="">
                 </a>
             </div >
