bae053a4ae8495fe252710cb2406ad39ed134c76
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-28 12:58:18 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -63,7 +63,7 @@
                             {{getDay(day.day_of_week)}}
                         </p>
                         <p class="col-xs-4 pull-right align_right disp_table_cell" v-if="!day.is_closed">
-                            {{day.open_time | moment("h a", timezone)}} -  {{day.close_time | moment("h a", timezone)}}
+                            {{day.open_time | moment("h a", timezone)}} - {{day.close_time | moment("h a", timezone)}}
                         </p>
                          <p class="col-xs-4 pull-right align_right disp_table_cell" v-else>
                             Closed
