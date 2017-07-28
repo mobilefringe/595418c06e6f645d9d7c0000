80a4d4f38de55a6d287ea7d7bb341b81918c8f13
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-28 14:35:22 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -237,7 +237,7 @@ define(["Vue", "jquery", "mm_mapsvg","mousewheel","raphael", "moment", "moment-t
         
                 map.setMarks([{ xy: [coords["x"]  - 25 + x_offset, coords["y"] -68 + y_offset],
                     attrs: {
-                        src: '//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1500567644000/map_pin_1x.pngg',   // image for marker
+                        src: '//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1500567644000/map_pin_1x.png',   // image for marker
                         href: '/stores/'+val.slug,
                         tooltip :val.name
                     },
