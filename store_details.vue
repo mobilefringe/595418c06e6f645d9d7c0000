610767a45cddfb54bac9def77eb1283a244668bb
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-28 11:34:24 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -30,7 +30,6 @@
         </div> 
         
         <div class="blue_box promo"  v-if="currentStore" @click="togglePromo">
-            
             <p class="dets_title">Promotions </p>
             <!--<p class="dets_data">{{currentStore.property_id}} - {{currentStore.store_hours_data.open_time | moment("h a", timezone)}}  {{currentStore.store_hours_data.close_time | moment("h a", timezone)}}</p>-->
             
