38ea861e391dc77c72590c86b33fd33bdf09f338
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-02 14:11:17 -0400

    The Toronto Path 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -3,7 +3,7 @@
         <div class="position_relative map_search_page">
             <div class="gray_bar">
                 <div class="col-xs-10">
-                <search-component :list="allStores" :suggestion-attribute="suggestionAttribute" @select="onOptionSelect1" class="search1" placeholder="Search Stores" keys="[name,category]">
+                <search-component :list="allStores" :suggestion-attribute="suggestionAttribute" @select="onOptionSelect1" class="search1" placeholder="Search Stores" :keys="search_key">
                     <template slot="item" scope="option">
                         <article class="media ">
                             <p>
