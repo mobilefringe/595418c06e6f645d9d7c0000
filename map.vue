4f9547316f0a62bd22c686b7f5fea583557b824c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-02 12:41:18 -0400

    The Toronto Path 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -78,7 +78,7 @@
                     <router-link v-bind:to="store1.path_url"><img src="//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1499180555000/Right Arrow.png" class="" alt=""></router-link>
                 </div>
                 <p class="dets_title">{{store1.name}} </p>
-                <p class="dets_data">{{store1.name}}</p>
+                <p class="dets_data">{{store1.property_id}}</p>
             </div>
             <div  v-show = "mobile_find_store" class="green_dets_box">
                 <div class="close_dets pull-left">
