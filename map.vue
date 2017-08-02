867e23559283e6a213fd9f85c1225144868fb549
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-02 14:52:10 -0400

    The Toronto Path 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -115,6 +115,7 @@
                 console.log("changed store1",this.mobile_search_store);
                 var val = this.store1;
                 var map= this.map;
+                map.marksHide();
                 if(val.svgmap_region != null && typeof(val.svgmap_region)  != 'undefined'){
                         var coords = map.get_coords(val.svgmap_region);
                         var height = parseInt(coords["height"])
