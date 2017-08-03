a387912d0ff7de96aa1847cafdc9650c84a71ee3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-03 10:30:45 -0400

    The Toronto Path 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -10,7 +10,7 @@
                 <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right hidden_now" alt="">
             </div>
              <hr>
-            <div  v-show = "show_dine" class="" id="dine_store_list">
+            <div  v-show = "true" class="" id="dine_store_list">
                 <div class="" id="dine_stores_container">
                     <div  v-for="promo in store_promo ">
                         <div class="row cat_name">
