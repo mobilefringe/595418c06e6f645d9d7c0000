b6b121b7023abec51843a27d625eeaf70003bd92
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-03 10:21:41 -0400

    The Toronto Path 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -16,7 +16,7 @@
                         <div class="row cat_name">
                             <router-link :to="{ name: 'mapStores', params: { id: index }}">
                                 <div class="col-xs-6 align_left">
-                                    {{promo.namw}}
+                                    {{promo.name}}
                                     <!--{{store.name}} -->
                                     <br/>
                                     <!--{{store.property_id}}-->
