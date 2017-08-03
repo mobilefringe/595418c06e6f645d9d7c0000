e13a4fe1e963b7e6b582e653e8f8d205962234e1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-03 12:02:06 -0400

    The Toronto Path 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -20,7 +20,7 @@
                     <img src="//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1501003074000/arrow_left_2x.png" class="" alt="">
                 </a>
             </div >
-            <p class="dets_title">{{currentStore.name | uppercase}} </p>
+            <p class="dets_title">{{currentPromo.name | uppercase}} </p>
             <p class="dets_data">
                 {{currentStore.property_id}} - {{currentStore.store_hours_today.open_time | moment("h a ", timezone)}}  {{currentStore.store_hours_today.close_time | moment("h a z", timezone)}}
             </p>
