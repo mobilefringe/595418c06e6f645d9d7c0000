06ac1da2c7718e1de0fffa6362b98f46694490f8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-03 10:05:08 -0400

    The Toronto Path 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="stores_page"> <!-- for some reason if you do not put an outer container div this component template will not render -->
         <div class="all_caps store_header">
-            Stores Directory
+            Promotions
         </div>
         <h4 class="all_caps home_title"> Discover The Toronto Path</h4>
         <div class="store_cat_listing">
