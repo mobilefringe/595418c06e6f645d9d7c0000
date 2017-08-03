c2fb56c8ded1521d9a9cb208d0645c2569d85240
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-03 10:12:34 -0400

    The Toronto Path 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -66,7 +66,7 @@
     return Vue.component("promos-component", {
       template: template, // the variable template will be injected
       created () {
-        var promo = _.groupBy(this.promotions, 'store.name');
+        var promo = _.groupBy(this.promotions, 'store.id');
        console.log(promo);
         }, 
       computed: {
