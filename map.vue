be0d8299d6101751b4bb8945c5d5f1c022f9f801
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-03 13:25:42 -0400

    The Toronto Path 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -185,8 +185,8 @@
                     $('.search1').offset( temp_cords2 );
                     $('.search2').offset( temp_cords1 );
                     var temp_placeholder =  $(".search1 input").attr("placeholder");
-                     $(".search1 input").attr("placeholder", $(".search2 input").attr("placeholder"));
-                     $(".search2 input").attr("placeholder",temp_placeholder );
+                    $(".search1 input").attr("placeholder", $(".search2 input").attr("placeholder"));
+                    $(".search2 input").attr("placeholder",temp_placeholder );
                     // $(".search1 input").attr("placeholder", "Search Stores");
                     // $(".search2 input").attr("placeholder", "Find a store near you");
                 }
