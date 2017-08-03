394c0864f5ffa0eb9ca3728be8b3c953962606f8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-03 10:42:45 -0400

    The Toronto Path 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -5,7 +5,7 @@
         </div>
         <h4 class="all_caps home_title"> Discover The Toronto Path</h4>
         <div class="store_cat_listing" v-for="(store_promo, index) in promo_by_stores">
-            <div class="all_caps cat_title" id="dine" v-on:click="toggle(store_promo)">
+            <div class="all_caps cat_title" id="dine" v-on:click="toggle(index)">
                 {{index}}
                 <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right hidden_now" alt="">
             </div>
