5fd5e614619bec27a666f8e3800b75dcd0e35f78
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-04 12:57:46 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -127,7 +127,7 @@ define(["Vue", "jquery", "mm_mapsvg","mousewheel","raphael", "moment", "moment-t
             }
         },
         created () {
-            console.log({{showLoader}})
+            console.log({{showLoader}});
             this.$emit('loadChange', true);
             console.log("emit to parent to make this true");
         },
