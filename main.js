28330e6a65385d99142d0facf4f4e77732599dcf
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-04 13:30:59 -0400

    The Toronto Path 1.0  /main.js

--- main.js
+++ main.js
@@ -138,17 +138,17 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
             changeLocale: function(val) {
                 this.locale = val; // this will update the data store, which in turn will trigger the watcher to update the locale in the system
             },
-            open_menu: function () {
+            open_menu () {
                 this.show_menu = true;
                 this.open_icon = false;
                 this.closed_icon = true;
             },
-            close_menu: function () {
+            close_menu () {
                 this.show_menu = false;
                 this.closed_icon = false;
                 this.open_icon = true;
             },
-        updateLoader(variable) {
+            updateLoader(variable) {
             this.showLoader = variable;
             console.log("changed showLoader to", this.showLoader);
         }
