29e631403472684c295b1ed708995fc1cac855a5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-04 13:29:32 -0400

    The Toronto Path 1.0  /main.js

--- main.js
+++ main.js
@@ -120,7 +120,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
             dataLoaded : function () {
                 this.showLoader = !this.dataLoaded;
                 console.log(this.showLoader);
-          }
+            }
         },
     computed: {
       // computed property for locale which returns locale value from data store and also updates the data store with new locale information
