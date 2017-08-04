aa4b9b59ddff39f6eae1f7e64cead1a9e5f29419
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-04 13:32:07 -0400

    The Toronto Path 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="home_page">
         <div class="gray_bar">
-            <div id="mobile_search"  class="col-xs-10">=
+            <div id="mobile_search"  class="col-xs-10">
                 <search-component :list="processedStores" :suggestion-attribute="suggestionAttribute" @select="onOptionSelect">
                     <template slot="item" scope="option">
                         <article class="media">
