485af23cf3861bc42f0986ad77dbfc35d186132c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-04 13:31:35 -0400

    The Toronto Path 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -6,11 +6,6 @@
                 <search-component :list="processedStores" :suggestion-attribute="suggestionAttribute" @select="onOptionSelect">
                     <template slot="item" scope="option">
                         <article class="media">
-                          <!--<figure class="media-left">
-                            <p class="image is-64x64">
-                              <img :src="option.data.store_front_url_abs">
-                            </p>
-                          </figure>-->
                             <p>
                                 <strong>{{ option.data.name }}</strong>
                             </p>
