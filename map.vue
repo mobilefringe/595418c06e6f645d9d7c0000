304dbc0920c307e569d56d122b83a81cf904d16d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-04 11:39:08 -0400

    The Toronto Path 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -205,10 +205,8 @@
                 this.store2 = null;
             },
             onOptionSelect1(option) {
-                console.log(this.search_one, $('.search1').offset());
                 if((this.search_one.left === $('.search1').offset().left) && (this.search_one.top === $('.search1').offset().top)) {
                     this.store1 = option;
-                    console.log("hi");
                 }
                 else {
                     this.store2 = option;
