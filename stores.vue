9776e873609186f569cb9ef8b6a4a342ffdd8c53
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-08 13:25:43 -0400

    The Toronto Path 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -65,7 +65,7 @@
             <hr>
             <div  v-show = "show_play" class="" id="shop_store_list">
                 <div class="" id="shop_stores_container">
-                    <div  v-for="(store, index) in shop_stores">
+                    <div  v-for="(store, index) in play_stores">
                         <div class="row cat_name">
                             <router-link :to="{ name: 'mapStores', params: { id:  index }}">
                                 <div class="col-xs-6 align_left">
