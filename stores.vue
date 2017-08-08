2f9219e5802b0537a5268fef4e0b1f4ae2a6619e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-08 10:01:04 -0400

    The Toronto Path 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -120,7 +120,7 @@
             var dine_cats = ["Food", "Fast Food", "Coffee and Cafe","Coffee and Cafes","Coffee and Café","Food Court Eatery", "Food Exchange","Food Services",
             "Restaurant","Restaurants", "Specialtu Food & Drink", "Specialty Food & Drink", "Specialty Food", "Specialy Food"];
             var shop_cats = [" Jewellery","Apparel","Accessories", "Accessories & Luggage", "Beauty Product","Bedding","Cards & Stationary","Cards, Stationary and Gifts","Children's Apparel", "Children's Wear", "Drugstore","Electronics","Electronics, Computers and Telephones", "Electronics, Computers, and Telephones","Fashion","Fashion & Accessories", "Fashion Accessories"];
-            var play_cats = [];
+            var play_cats = ["ATTRACTIONS"];
             var stay_cats = [];
             var service_cats = [];
             console.log(cats);
