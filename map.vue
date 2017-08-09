89729200317a846ac86238cb261201813bd324ab
ikvir@mobilefringe.com @ CodeCloud <ikvir@mobilefringe.com @ CodeCloud>
2017-08-09 19:30:29 -0400

    The Toronto Path 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -74,9 +74,10 @@
                 search_two : null
             }
         },
+        created (){
+          window.Raphael = Raphael; // our mapSvg plugin is stupid and outdated. need this hack to tie Raphael to window object (global variable)
+        },
         mounted: function (){
-            console.log(Raphael);
-            var Raphael = Raphael;
             var map = $('#mapsvg').mapSvg({
                 source: this.getSVGurl,    // Path to SVG map
                 colors: {stroke: '#aaaaaa', selected: "#CC00CC", hover: "#CC00CC"},
