60ca9585da58c7e07814cea7d3543768ddc7b764
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-09 13:28:28 -0400

    The Toronto Path 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -59,7 +59,7 @@
                 <hr>
             </div>
             <div class="all_caps cat_title" id="play" v-on:click="toggle('play')">
-                Play
+                {{$t("message.play")}}
                 <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right hidden_now" alt="">
             </div>
             <hr>
