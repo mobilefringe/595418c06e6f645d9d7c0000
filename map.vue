72a7fd79b32f7d7bcd6ca2e65d6afe28fdacafae
ikvir@mobilefringe.com @ CodeCloud <ikvir@mobilefringe.com @ CodeCloud>
2017-08-09 19:27:19 -0400

    The Toronto Path 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -76,6 +76,7 @@
         },
         mounted: function (){
             console.log(Raphael);
+            window.Raphael = Raphael;
             var map = $('#mapsvg').mapSvg({
                 source: this.getSVGurl,    // Path to SVG map
                 colors: {stroke: '#aaaaaa', selected: "#CC00CC", hover: "#CC00CC"},
