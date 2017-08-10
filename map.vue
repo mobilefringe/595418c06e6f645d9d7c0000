0906fe34f9d4915d7df64351dd1babb64d94b539
ikvir@mobilefringe.com @ CodeCloud <ikvir@mobilefringe.com @ CodeCloud>
2017-08-10 14:26:56 -0400

    The Toronto Path 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -81,7 +81,7 @@
           window.Raphael = Raphael; // our mapSvg plugin is stupid and outdated. need this hack to tie Raphael to window object (global variable)
         },
         mounted: function (){
-            let map = $('#mapsvg').mapSvg({
+            var map = $('#mapsvg').mapSvg({
                 source: this.getSVGurl,    // Path to SVG map
                 colors: {stroke: '#aaaaaa', selected: "#CC00CC", hover: "#CC00CC"},
                 // viewBox: [3000,0,6000,6000],
