7ce501c35a3388d79d23cfd69a490ae5e3cc81b2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-10 12:57:02 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -24,7 +24,7 @@
                 <i class="fa fa-globe col-xs-1" aria-hidden="true"></i>
                 <p class="wrap col-xs-10"><a :href="currentStore.website">{{currentStore.website}}</a></p>
             </div>
-            <div class="row">
+            <div class="row" v-show="currentStore.website">
                 <i class="fa fa-phone col-xs-1" aria-hidden="true"></i>
                 <p class="wrap col-xs-10"><a :href="'tel:' +currentStore.phone">{{currentStore.phone}}</a></p>
                 
