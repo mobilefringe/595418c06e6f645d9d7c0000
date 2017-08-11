1465d3ab00537ea1534e7a27049c455eca60c430
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-11 15:37:33 -0400

    The Toronto Path 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -41,7 +41,7 @@
         // props: ['menu_items'],
         template: template, // the variable template will be injected
          props: {
-            menu_items: type: Array,
+            menu_items: type: Array
                 
          }.
         data: function() {
