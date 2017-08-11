9bcf34545c375f281db85628b120ba0c4995a9db
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-11 10:21:01 -0400

    The Toronto Path 1.0  /svg_map.vue

--- svg_map.vue
+++ svg_map.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="svg-map" >
         <div class="map">
-            <div class="demo_1 map3" style="" id="mapsvg"></div>
+            <div class="demo_1 map3" id="mapsvg"></div>
         </div>
     </div>
 </template>
