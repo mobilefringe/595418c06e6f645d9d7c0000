188d703d74da7bf65a1646d94415fb8b21afb86f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-14 12:08:57 -0400

    The Toronto Path 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -219,6 +219,11 @@
                 return myArr.filter((obj, pos, arr) => {
                     return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
                 });
+            },
+            updateSVGMap (map) {
+                
+                this.map = map;
+                console.log("updating map in svg_map",this.map );
             }
         }
     })
