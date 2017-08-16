2cb3444ca0265227c0bc5deaa0875fcc09f2846d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-16 11:54:18 -0400

    The Toronto Path 1.0  /svg_map.vue

--- svg_map.vue
+++ svg_map.vue
@@ -162,6 +162,7 @@
                     console.log("mark Id is " ,mark_id);
                     var filtered_set = _.filter(this.all_markers, function(o) { return (o.id !== mark_id) });
                     console.log("filtered Rmarks ", filtered_set);
+                    
                     // this.map.marksReset(filtered_set);
                     // return _.filter(marks, function(o) { return (o.0.id == mark_id) })[0];
                     
