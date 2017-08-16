a89cff4d818012b533e42e83826a92b678fb8fc0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-16 10:20:47 -0400

    The Toronto Path 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -127,12 +127,13 @@
             },
             store2 : function () {
                 
-                 if (this.store1.marker == null && this.store1.marker == undefined) {
+                 var mark = this.svgMapRef.addMarker(this.store2,'//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1501693321000/map_pin_green_1x.png');
+                 
+                if (this.store2.marker == null && this.store2.marker == undefined) {
                     var mark = this.svgMapRef.addMarker(this.store1,'//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1500567644000/map_pin_1x.png');
-                    if( mark !== null && mark != undefined) {
-                        console.log("store marker container",this.store1.marker);
-                        Vue.set(this.store1,'marker',mark[0]);
-                    }
+                    if( this.store2.marker=== null && this.store2.marker=== undefined && mark !== null && mark != undefined) {
+                    Vue.set(this.store2,'marker',mark[0]);
+                }
                    console.log("the mark is" ,this.store1);
                 }
                 
