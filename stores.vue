123e3c195475181bbcaf43344aa207a98564b29e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-17 12:00:32 -0400

    The Toronto Path 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -14,7 +14,7 @@
                 <div class="" id="dine_stores_container">
                     <div  v-for="(store, index) in dine_stores ">
                         <div class="row cat_name" @click="getRoute(store)">
-                            <router-link :to="getRoute(store)">
+                            <!--<router-link :to="getRoute(store)">-->
                                 <div class="align_center">
                                     {{index}}
                                 </div>
