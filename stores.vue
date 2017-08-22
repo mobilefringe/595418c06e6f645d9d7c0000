75ae2397aa50075b92e074ff2e6c3426eac1c41b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-22 12:27:06 -0400

    The Toronto Path 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -61,7 +61,7 @@
             </div>
             <div class="all_caps cat_title" id="service" v-on:click="toggle('service')">
                 {{$t("message.service")}}
-                <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right " alt="" v-show="show_dine">
+                <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right " alt="" v-show="show_service">
             </div>
              <hr>
              <div  v-show = "show_service" class="" id="service_store_list">
@@ -79,7 +79,7 @@
             </div>
             <div class="all_caps cat_title" id="stay" v-on:click="toggle('stay')">
                 {{$t("message.stay")}}
-                <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right " alt="" v-show="show_dine">
+                <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right " alt="" v-show="show_stay">
             </div>
              <hr>
              <div  v-show = "show_stay" class="" id="stay_store_list">
