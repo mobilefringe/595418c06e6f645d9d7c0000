ecbd0ef9bdc3390811c15068214c363eca8034eb
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-22 12:27:17 -0400

    The Toronto Path 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -25,7 +25,7 @@
             </div>
             <div class="all_caps cat_title" id="shop" v-on:click="toggle('shop')">
                 {{$t("message.shop")}}
-                <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right " alt="" v-show="show_dine">
+                <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right " alt="" v-show="show_shop">
             </div>
             <hr>
             <div  v-show = "show_shop" class="" id="shop_store_list">
