83182d63fb435209c87d2eb22ad2bc9dfc07d3ed
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-24 09:39:15 -0400

    The Toronto Path 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -10,7 +10,7 @@
             </div >
             <p class="dets_title">{{currentStore.name | uppercase}} </p>
             <p class="dets_data">
-                {{currentStore.property_id}} 
+                {{currentStore.property}} 
                 <span v-if="currentStore.todays_hour != null"> - {{currentStore.store_hours_today.open_time | moment("h a ", timezone)}}  {{currentStore.store_hours_today.close_time | moment("h a z", timezone)}}</span>
             </p>
         </div>
