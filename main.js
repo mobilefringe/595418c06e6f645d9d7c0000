da6ba25ffe15e508d8c14726ba49a8d29906368a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-25 15:19:49 -0400

    The Toronto Path 1.0  /main.js

--- main.js
+++ main.js
@@ -110,7 +110,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
             this.dataLoaded = true;
         }, error => {
             console.error("Could not retrieve data from server. Please check internet connection and try again.");
-      });
+        });
     },
     watch: {
       // watcher to update vue-i18n when the locale has been changed by the user
