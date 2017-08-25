74b4c25ca4ff80a43f1fbe1e310dd20556b258ff
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-25 15:38:19 -0400

    The Toronto Path 1.0  /notfoundcomponent.vue

---
+++ notfoundcomponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+    <div class="large-6 columns">
+      <h1>{{text}}</h1>
+      <img src="http://assets.codecloudapp.com/sites/5890c5296e6f640dd9000000/image/png/1463595344000/404_error_msg-1.png">
+    </div>
+  </div>
+</template>
+
+<script>
+  define(["Vue"], function(Vue) {
+    return Vue.component("not-found-component", {
+      template: template, // the variable template will be injected
+      data: function() {
+        return {"text": "Custom 404 Page"};
+      }
+    });
+  });
+</script>
