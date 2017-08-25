968c953e0dc8c1ab4f0a8349300bb1e981dfe5b2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-25 15:25:44 -0400

    The Toronto Path 1.0  /map_stores.vue

--- map_stores.vue
+++ map_stores.vue
@@ -65,7 +65,6 @@
                 var temp_ref = this.svgMapRef;
                 _.delay(function() {
                     _.forEach(temp, function(val, key) {
-                        // console.log("hello");
                         temp_ref.addMarker(val,'//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1500567644000/map_pin_1x.png');
                        
                     });
