f1c7f8c8d23f05e0164a5b1dde0a29367c765428
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-25 15:40:46 -0400

    The Toronto Path 1.0  /map_stores.vue

--- map_stores.vue
+++ map_stores.vue
@@ -17,7 +17,7 @@
 </template>
 
 <script>
-  define(["Vue", "jquery", "Raphael", "mm_mapsvg","mousewheel","vue!svg-map"], function(Vue, $,Raphael,mapsvg,mousewheel,SVGMapComponent) {
+    define(["Vue", "jquery", "Raphael", "mm_mapsvg","mousewheel","vue!svg-map"], function(Vue, $,Raphael,mapsvg,mousewheel,SVGMapComponent) {
     return Vue.component("map-store-component", {
         template: template, // the variable template will be injected
         data: function() {
