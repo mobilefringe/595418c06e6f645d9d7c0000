63af146369d021b5e161777cfc5f2da2298b22e4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-25 15:44:19 -0400

    The Toronto Path 1.0  /routes.js

--- routes.js
+++ routes.js
@@ -1,66 +1,66 @@
 define([], function () {
-  return [
-    {
-      path: '/',
-      component: view('home')
-    },
-    {
-      path: '/stores',
-      component: view('default'),
-      children: [
+    return [
         {
-          path: '',
-          component: view('stores'),
-          name: 'storeList'
+          path: '/',
+          component: view('home')
         },
         {
-          path: ':id',
-          component: view('store_details'),
-          name: 'storeDetails'
-        }
-      ]
-    },
-    {
-      path: '/promotions',
-      component: view('default'),
-      children: [
-        {
-          path: '',
-          component: view('promotions'),
-          name: 'storeList'
+          path: '/stores',
+          component: view('default'),
+          children: [
+            {
+              path: '',
+              component: view('stores'),
+              name: 'storeList'
+            },
+            {
+              path: ':id',
+              component: view('store_details'),
+              name: 'storeDetails'
+            }
+          ]
         },
         {
-          path: ':id',
-          component: view('promotion_details'),
-          name: 'promotionDetails'
-        }
-      ]
-    },
-    {
-      path: '/map',
-      component: view('default'),
-      children: [
-        {
-          path: '',
-          component: view('map'),
-          name: 'map'
+          path: '/promotions',
+          component: view('default'),
+          children: [
+            {
+              path: '',
+              component: view('promotions'),
+              name: 'storeList'
+            },
+            {
+              path: ':id',
+              component: view('promotion_details'),
+              name: 'promotionDetails'
+            }
+          ]
         },
         {
-          path: ':id',
-          component: view('map_stores'),
-          name: 'mapStores'
+          path: '/map',
+          component: view('default'),
+          children: [
+            {
+              path: '',
+              component: view('map'),
+              name: 'map'
+            },
+            {
+              path: ':id',
+              component: view('map_stores'),
+              name: 'mapStores'
+            }
+          ]
+        },
+        {
+          path: '/404',
+          name: '404',
+          component: view('notfoundcomponent')
+        },
+        {
+          path: '*',
+          redirect: '/404'
         }
-      ]
-    },
-    {
-      path: '/404',
-      name: '404',
-      component: view('notfoundcomponent')
-    },
-    {
-      path: '*',
-      redirect: '/404'
-    }
   ]
 
   /**
