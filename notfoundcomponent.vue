599880b8e8b303aed26975202d735d3c4215b77b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-25 15:38:37 -0400

    The Toronto Path 1.0  /notfoundcomponent.vue

--- notfoundcomponent.vue
+++ notfoundcomponent.vue
@@ -2,7 +2,7 @@
   <div class="row">
     <div class="large-6 columns">
       <h1>{{text}}</h1>
-      <img src="http://assets.codecloudapp.com/sites/5890c5296e6f640dd9000000/image/png/1463595344000/404_error_msg-1.png">
+      <img src="/codecloud.cdn.speedyrails.net/sites/5890c5296e6f640dd9000000/image/png/1463595344000/404_error_msg-1.png">
     </div>
   </div>
 </template>
