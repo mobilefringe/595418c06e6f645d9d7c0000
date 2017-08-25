98a09a6663884fa41fc3cd20b51908afd37c7458
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-25 15:49:37 -0400

    The Toronto Path 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -77,7 +77,7 @@
                 </div>
                 <hr>
             </div>
-            <div class="all_caps cat_title" id="stay" @click="show_shop = !show_shop">
+            <div class="all_caps cat_title" id="stay" @click="show_stay = !show_stay">
                 {{$t("message.stay")}}
                 <img src="//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498596224000/up_arrow_2x.png" class="up_arrow pull-right " alt="" v-show="show_stay">
             </div>
